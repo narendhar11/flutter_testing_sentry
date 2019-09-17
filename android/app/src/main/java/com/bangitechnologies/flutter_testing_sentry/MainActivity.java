2e881affbb00f098624c9a565a3eccbb5009f2e6
cnarendhar <narendhar.chiluka@bandgitechnologies.com>
2019-09-17 11:21:38 +0530

    First commit

---
+++ android/app/src/main/java/com/bangitechnologies/flutter_testing_sentry/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bangitechnologies.flutter_testing_sentry;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
